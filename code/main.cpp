ec8986cb73fcf43cbb414b054ebdd4f085eac1f1
mbeccari856608 <m.beccari@campus.unimib.it>
2024-10-11 03:13:17 +0200

    initial commit

---
+++ code/main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <stdint.h>
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+
+int main()
+{
+	auto a = 4;
+	std::cout << "Hello World";
+
+	return 0;
+}
