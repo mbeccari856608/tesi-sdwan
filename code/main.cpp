0777a19c11d09b9451260cd7e97d535284abb2d2
mbeccari856608 <m.beccari@campus.unimib.it>
2024-10-15 02:09:15 +0200

    wip

--- code/main.cpp
+++ code/main.cpp
@@ -1,18 +1,12 @@
-
-#include <iostream>
-#include <stdint.h>
 #include "ns3/core-module.h"
-#include "ns3/network-module.h"
-#include "ns3/csma-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/applications-module.h"
-#include "ns3/ipv4-global-routing-helper.h"
 
-int main()
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("HelloSimulator");
+
+int main(int argc, char* argv[])
 {
-	auto a = 4;
-	std::cout << "Hello World";
-
-	return 0;
+    std::cout << "yoo";
+    NS_LOG_UNCOND("Hello Simulator");
+    return 0;
 }
